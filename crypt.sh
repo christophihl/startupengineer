0c235e47189eddc47ea202d292558f6e8005d479
Christoph Ihl <christoph.ihl@tuhh.de>
2020-04-11 20:16:30 +0200

    change encrypting title & instruction

--- crypt.sh
+++ crypt.sh
@@ -5,4 +5,4 @@ source ./config/_default/credentials
 
 npm install -g https://github.com/christophihl/staticrypt
 
-find ./public/_syllabi/tech_ent -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded" | openssl enc -d -base64) -o {} \;
+find ./public/_syllabi/tech_ent -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded" | openssl enc -d -base64) -o {} -t {Access to Course Notes & Materials} -i {Enter the Course Password}\;
