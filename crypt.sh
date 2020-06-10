c912fe6cee5592477aa2c525bb13468968b33650
Christoph Ihl <christoph.ihl@tuhh.de>
2020-06-10 22:40:45 +0200

    pw update

--- crypt.sh
+++ crypt.sh
@@ -5,8 +5,8 @@ source ./config/_default/credentials
 
 npm install -g https://github.com/christophihl/staticrypt
 
-find ./public/_syllabi/tech_ent/z_cases -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
+find ./public/_repos/tech_ent/z_cases -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
 
-find ./public/_syllabi/gbwl -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_1" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
+find ./public/_repos/gbwl -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_1" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
 
 
