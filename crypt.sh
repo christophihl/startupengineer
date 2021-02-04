8cbb6fbcda94354230a8b367a3d35af1c824bfdf
Christoph Ihl <christoph.ihl@tuhh.de>
2021-02-04 11:12:05 +0100

    update courses

--- crypt.sh
+++ crypt.sh
@@ -5,9 +5,9 @@ source ./config/_default/credentials
 
 npm install -g https://github.com/christophihl/staticrypt
 
-find ./public/_repos/tech_ent -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
+find ./public/_repos/tech_ent -maxdepth 1 -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
 
-find ./public/_repos/gbwl -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_1" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
+find ./public/_repos/gbwl -maxdepth 1 -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_1" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
 
 find ./public/_repos/dat_sci_1 -maxdepth 1 -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_2_1" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
 find ./public/_repos/dat_sci_2 -maxdepth 1 -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded_2_2" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
