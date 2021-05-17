f900c26bf314604d9cd57b2f7d8fa069c5908dfb
jwarz <joschka.schwarz@tuhh.de>
2021-05-17 11:20:46 +0200

    revert staticrypt to christpoh

--- crypt.sh
+++ crypt.sh
@@ -3,7 +3,7 @@ set -e
 
 source ./config/_default/credentials
 
-npm install -g https://github.com/jwarz/staticrypt
+npm install -g https://github.com/christophihl/staticrypt
 
 find ./public/_repos/tech_ent -maxdepth 1 -type f -name "index.html" -exec staticrypt {} $(echo "$password_encoded" | openssl enc -d -base64) -o {} -t "Access to Course Materials" -i "Enter the Course Password" \;
 
